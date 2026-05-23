455e3222fdd53f9037db6573a777d38b0e0b2475
phamdevctpmq <phamminhduc0865713395@gmail.com>
2026-05-23 22:32:58 +0700

    ok

---
+++ src/store/authStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useAuthStore = create((set) => ({
+  token: null,
+  user: null,
+  setAuth: async (token, username, roles, refreshToken) => {
+    await AsyncStorage.setItem("token", token);
+    await AsyncStorage.setItem("refreshToken", refreshToken);
+    set({ token, user: { username, roles } });
+  },
+  logout: async () => {
+    await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("refreshToken");
+    set({ token: null, user: null });
+  },
+  loadToken: async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) set({ token });
+  },
+}));
